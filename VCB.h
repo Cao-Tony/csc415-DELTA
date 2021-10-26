b38ecbab4c9b5d1dd47ccd1f74b9fcb555b32872
Student <tcao@mail.sfsu.edu>
2021-10-25 17:24:24 -0700

    VCB init updated

--- VCB.h
+++ VCB.h
@@ -7,24 +7,6 @@
 
 int initializeVCB(u_int64_t volumeSize, u_int64_t blockSize);
 
-int getPlaceValue(unsigned int number);
-
-int openVCB(int blockSize);
-
-void closeVCB();
-
-int changeFSC(int blocks);
-
-unsigned int getFSC();
-
-unsigned int getTBC();
-
-unsigned int getSOB();
-
 void printVCB();
-
-void setRootDirectory(unsigned int);
-
-unsigned int getRootDirectory();
 
 #endif
