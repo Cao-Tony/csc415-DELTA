af87f115b3b7dd7fdbd104a2c0dedae95cb24ed7
kbishop1-sfsu <78761220+kbishop1-sfsu@users.noreply.github.com>
2021-11-30 12:40:10 -0800

    updating comment block

--- b_io.c
+++ b_io.c
@@ -1,8 +1,8 @@
 /**************************************************************
 * Class:  CSC-415 
-* Name: Tony Cao, Dominique Dutton
-* Student ID: 920171613 (Tony), 920820781 (Dominique)
-* GitHub Handle:  Cao-Tony
+* Name: Tony Cao, Dominique Dutton, Kandace Bishop
+* Student ID: 920171613 (Tony), 920820781 (Dominique), 918762889 (Kandace)
+* GitHub Handle:  Cao-Tony, kbishop1-sfsu
 * Project: File System
 *
 * File: b_io.c
