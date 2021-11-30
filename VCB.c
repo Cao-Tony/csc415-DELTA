e3ac08a40a1e517beabc49029b594bca4d02df4c
kbishop1-sfsu <78761220+kbishop1-sfsu@users.noreply.github.com>
2021-11-30 12:38:20 -0800

    updating comment block

--- VCB.c
+++ VCB.c
@@ -1,8 +1,8 @@
 /**************************************************************
 * Class:  CSC-415 
-* Name: Tony Cao
-* Student ID: 920171613
-* GitHub Handle:  Cao-Tony
+* Name: Tony Cao, Dominique Dutton, Kandace Bishop
+* Student ID: 920171613 (Tony), 920820781 (Dominique), 918762889 (Kandace)
+* GitHub Handle:  Cao-Tony, kbishop1-sfsu
 * Project: File System
 *
 * File: VCB.c
